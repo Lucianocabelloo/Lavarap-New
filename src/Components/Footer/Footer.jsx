b98f064fed6c7b527c5a25a6dd7132da6bccc8a3
lucianocabelloo1~ <you@example.com>
2023-08-04 17:08:43 -0300

    Feature Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <div className='h-[5vh] bg-[#3F4862]'>
-        <h2 className=" h-full font-bold text-white grid place-content-center">© Derechos Reservados a Elece Dev</h2>
+        <h2 className=" h-full font-bold text-white grid place-content-center">© Todos Derechos Reservados a Elece Dev</h2>
     </div>
   )
 }
